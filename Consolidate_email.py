c09a86a88b982fcc00275bc631e2515f6319af3d
shashi <33113076+shashiRS@users.noreply.github.com>
2025-02-12 13:03:18 +0530

    Update Consolidate_email.py

--- Consolidate_email.py
+++ Consolidate_email.py
@@ -251,7 +251,7 @@ if __name__ == "__main__":
     body_html = body_html.format(
     url_link=txt_dict['url'],
     datetime=str(datetime.datetime.now()),
-    sw_version=['Subject under Test'],
+    sw_version=project_table_dict['Subject under Test'],
     simulation_github_url=txt_dict['simulation_repo'],
     validation_github_url=txt_dict['validation_repo'],
     contest_github_url=txt_dict['Contest_repo'],
